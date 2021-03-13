0aa3077e5ca4afef9f804f78ee3529b70859e9a4
Evans357 <80275455+Evans357@users.noreply.github.com>
2021-03-13 13:37:19 +0300

    Add files via upload

---
+++ urok2_zadacha_4.py
@@ -0,0 +1,5 @@
+my_list = ['инженер-конструктор Игорь', 'главный бухгалтер МАРИНА', 'токарь высшего разряда нИКОЛАй', 'директор аэлита']
+for i in range(len(my_list)):  # 
+    my_list[i] = my_list[i].split()
+    my_list[i][-1] = str(my_list[i][-1].capitalize())
+    print(f'Привет,{my_list[i][-1]}!')
